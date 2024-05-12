trying to fix TrajectoryDesiredStateSource to work with variable dt and also fixed kM sign?

main.py
@@ -17,7 +17,6 @@ from pydrake.all import (
     namedview,
     JointSliders
 )
-from pydrake.examples import QuadrotorGeometry, QuadrotorPlant, StabilizingLQRController
 from underactuated.scenarios import AddFloatingRpyJoint
 
 import numpy as np
@@ -27,7 +26,7 @@ import argparse
 import yaml
 
 from src.utils import *
-from src.ddp import solve_trajectory, solve_trajectory_fixed_timesteps_fixed_interval, TrajectoryDesiredStateSource
+from src.ddp import solve_trajectory, solve_trajectory_fixed_timesteps_fixed_interval
 from src.se3_leaf import SE3Controller
 
 meshcat = StartMeshcat()
@@ -44,7 +43,7 @@ rx0 = 0.0
 ry0 = 0.0
 rz0 = 0.0
 
-pose_goal = np.array([0, 0, 2.0, 3.14, 0, 3.14])
+pose_goal = np.array([0, 0, 2.0, 3.14, 0, 1.57])
 
 
 ################################################################################
@@ -72,11 +71,12 @@ plant.Finalize()
 # Set up the propellers to generate spatial force on quadrotor
 body_index = plant.GetBodyByName("base_link").index()
 kF = 1.0  # Force input constant
+# Propellors 1 and 3 rotate CCW, 2 and 4 rotate CW
 prop_info = [
-    PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), L/np.sqrt(2), 0]), kF, kM),
-    PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), L/np.sqrt(2), 0]), kF, -kM),
-    PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, kM),
-    PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, -kM),
+    PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), L/np.sqrt(2), 0]), kF, -kM),
+    PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), L/np.sqrt(2), 0]), kF, kM),
+    PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, -kM),
+    PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, kM),
 ]
 propellers = builder.AddSystem(Propeller(prop_info))
 builder.Connect(
@@ -100,7 +100,7 @@ builder.Connect(
 #     se3_controller.GetInputPort("drone_state")
 # )
 # builder.Connect(
-#     desired_state_source.GetOutputPort("trajectory_desired_state")
+#     desired_state_source.GetOutputPort("trajectory_desired_state"),
 #     se3_controller.GetInputPort("x_trajectory")
 # )
 # builder.Connect(
@@ -150,10 +150,11 @@ plant.GetJointByName("rz").set_angle(plant_context, rz0)  # Yaw
 ################################################################################
 # Solve for trajectory
 N=20
-x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace, dt, final_translation_error, final_rotation_error = solve_trajectory(plant.get_state_output_port().Eval(plant_context), pose_goal, N)
+x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace, dt, dt_array, final_translation_error, final_rotation_error = solve_trajectory(plant.get_state_output_port().Eval(plant_context), pose_goal, N)
 # x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace = solve_trajectory_fixed_timesteps(plant.get_state_output_port().Eval(plant_context), pose_goal, N)
 
 print(f"{dt=}\n")
+print(f"{dt_array=}\n")
 print(f"{x_trj=}\n")
 print(f"{u_trj=}\n")
 print(f"{cost_trace=}\n")
@@ -167,7 +168,7 @@ pos_3d_matrix = x_trj[:,:3].T
 # print(f"{pos_3d_matrix.T=}")
 meshcat.SetLine("ddp traj", pos_3d_matrix)
 
-# desired_state_source.set_time_interval(dt)
+# desired_state_source.set_time_intervals(dt_array)
 # desired_state_source.GetInputPort("trajectory").FixValue(x_trj)
 
 

src/ddp.py
@@ -1,8 +1,6 @@
 from pydrake.all import (
     RollPitchYaw,
     RigidTransform,
-    AbstractValue,
-    BasicVector,
 )
 
 import matplotlib as mpl
@@ -55,6 +53,13 @@ def continuous_dynamics(x, u):
     return np.concatenate((p_dot, v_dot.flatten(), R_dot.flatten(), W_dot)), R, W
 
 
+def compute_discrete_dynamics_time_step(n, dt_nominal):
+    # https://www.desmos.com/calculator/vegofqyne7
+    beta = 5
+    dt = 1/(beta*n + (1/dt_nominal)) + dt_nominal
+    return dt
+
+
 def discrete_dynamics(x, u, n, dt_nominal):
     """
     Calculates next state based on current state and control input, using
@@ -64,14 +69,9 @@ def discrete_dynamics(x, u, n, dt_nominal):
 
     x is the (18,) np vector containing the current state.
     """
-    # https://www.desmos.com/calculator/vegofqyne7
-    beta = 5
-    dt = 1/(beta*n + (1/dt_nominal)) + dt_nominal
+    dt = compute_discrete_dynamics_time_step(n, dt_nominal)
 
     x_dot, R, W = continuous_dynamics(x, u)
-
-    # x_new = x_dot*dt + x  # Euler Integration
-    # return x_new
 
     # Precise integration of rotation matrix to preserve orthogonality and det=1 using Exponential Map
     theta = np.linalg.norm(W + eps)  # add eps to prevent input to np.linalg.norm from going to 0 (np.linalg.norm becomes undifferentiable)
@@ -442,7 +442,8 @@ def solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, dt, max_it
 
 def solve_trajectory(x0, pose_goal, N):
     """
-    Perform Linear Search in time dimension to find optimal number of time steps.
+    Run iLQR to generat an optimal trajectory, while performing Linear Search to
+    find optimal time interval dt.
     """
     min_cost = np.inf
     min_cost_traj = []
@@ -463,33 +464,9 @@ def solve_trajectory(x0, pose_goal, N):
     # Error from taking norm of e_R (equation (8)) from Lee et al.
     final_rotation_error = np.linalg.norm(0.5 * vee_map(R_goal.T @ R - R.T @ R_goal))
 
-    return *min_cost_traj, min_cost_time_interval, final_translation_error, final_rotation_error
+    # Generate list of time steps corresponding to each x and u in the trajectory
+    dt_array = np.array([])
+    for n in range(N):
+       np.append(dt_array, compute_discrete_dynamics_time_step(n, min_cost_time_interval))
 
-
-class TrajectoryDesiredStateSource(LeafSystem):
-    def __init__(self):
-        LeafSystem.__init__(self)
-
-        # Input port for x_traj, computed by ddp
-        traj = AbstractValue.Make(np.array([]))
-        self.DeclareAbstractInputPort("trajectory", traj)
-        
-        # Define output port for desired drone state in SE(3) form:
-        # [x, y, z, x_dot, y_dot, z_dot, R1, R2, R3, R4, R5, R6, R7, R8, R9, W1, W2, W3].T
-        self.output_port_drone_state_se3 = self.DeclareVectorOutputPort("trajectory_desired_state",
-                                                                           BasicVector(18),
-                                                                           self.CalcOutput)
-        
-        self.dt = None
-
-    def set_time_interval(self, dt):
-        self.dt = dt
-        
-    def CalcOutput(self, context, output):
-        """
-        Simply convert the state representation and set the output
-        """
-        traj = self.get_input_port(0).Eval(context)
-
-        desired_state = traj[int(context.get_time() / self.dt)]
-        output.SetFromVector(desired_state)
+    return *min_cost_traj, min_cost_time_interval, dt_array, final_translation_error, final_rotation_error

src/utils.py
@@ -3,6 +3,7 @@ from pydrake.all import (
     Diagram,
     LeafSystem,
     BasicVector,
+    AbstractValue,
 )
 import pydrake.symbolic as sym
 
@@ -14,7 +15,7 @@ import numpy as np
 # Quadrotor Constants (derived from quadrotor MultibodyPlant)
 m = 0.775       # quadrotor mass
 L = 0.15        # distance from the center of mass to the center of each rotor in the b1, b2 plane
-kM = 0.0245     # relates propellers' torque generated to thrust generated
+kM = 0.0245     # relates moment applied to quadrotor to the thurst generated
 g = -9.81       # gravity
 I = np.array([[1.50000000e-03, 0.00000000e+00, 2.02795951e-16],
               [0.00000000e+00, 2.50000000e-03, 0.00000000e+00],
@@ -134,3 +135,46 @@ class StateConverter(LeafSystem):
 
         # Set output
         output.SetFromVector(drone_state_se3)
+
+
+
+class TrajectoryDesiredStateSource(LeafSystem):
+    def __init__(self):
+        LeafSystem.__init__(self)
+
+        # Input port for x_traj, computed by ddp
+        traj = AbstractValue.Make(np.array([]))
+        self.DeclareAbstractInputPort("trajectory", traj)
+        
+        # Define output port for desired drone state in SE(3) form:
+        # [x, y, z, x_dot, y_dot, z_dot, R1, R2, R3, R4, R5, R6, R7, R8, R9, W1, W2, W3].T
+        self.output_port_drone_state_se3 = self.DeclareVectorOutputPort("trajectory_desired_state",
+                                                                           BasicVector(18),
+                                                                           self.CalcOutput)
+        
+        self.dt_array = None
+        self.n = 0
+        self.traj_elapsed_time = 0
+
+
+    def set_time_intervals(self, dt_array):
+        self.dt_array = dt_array
+        self.N = np.shape(dt_array)[0]
+        
+
+    def CalcOutput(self, context, output):
+        """
+        Simply convert the state representation and set the output
+        """
+        traj = self.get_input_port(0).Eval(context)
+
+        t = context.get_time()
+
+        self.n = min(self.n, self.N)
+
+        if t > self.traj_elapsed_time + self.dt_array[self.n]:
+            self.traj_elapsed_time += self.dt_array[self.n]
+            self.n += 1
+
+        desired_state = traj[self.n]
+        output.SetFromVector(desired_state)