536e341009b3c13502a20c6b002a73a96a79b0c2
Michaelszeng <michaelszeng@gmail.com>
2024-05-12 15:29:21 -0400

    non-differentiability of recursive base case issue

--- src/ddp.py
+++ src/ddp.py
@@ -55,35 +55,97 @@ def continuous_dynamics(x, u):
     return np.concatenate((p_dot, v_dot.flatten(), R_dot.flatten(), W_dot)), R, W
 
 
-def discrete_dynamics(x, u, n, dt_nominal):
+# def discrete_dynamics(x, u, dt, num_steps):
+#     """
+#     Calculates next state based on current state and control input, using
+#     continuous dynamics.
+
+#     u is a (4,) np vector of propeller forces.
+
+#     x is the (18,) np vector containing the current state.
+
+#     dt is the time step for Euler Intgration.
+
+#     num_steps is the number of steps (of length dt) to roll forward.
+#     """
+#     for i in range(num_steps):
+#         x_dot, R, W = continuous_dynamics(x, u)
+
+#         # Precise integration of rotation matrix to preserve orthogonality and det=1 using Exponential Map
+#         theta = np.linalg.norm(W + eps)  # add eps to prevent input to np.linalg.norm from going to 0 (np.linalg.norm becomes undifferentiable)
+#         W_hat = hat_map(W) * dt
+#         # Rodrigues' Formula
+#         R_new = R @ (np.eye(3) + np.sin(theta) * W_hat + (1 - np.cos(theta)) * np.power(W_hat, 2))
+
+#         # Euler Integration for other components of state
+#         x =  np.concatenate((x[:6]+x_dot[:6]*dt, R_new.flatten(), x[15:]+x_dot[15:]*dt))
+        
+#     return x
+
+
+def discrete_dynamics(x, u, dt, num_steps):
     """
-    Calculates next state based on current state and control input, using
-    continuous dynamics.
+    Recursively calculates next state based on current state and control input, 
+    using continuous dynamics.
 
     u is a (4,) np vector of propeller forces.
 
     x is the (18,) np vector containing the current state.
+
+    dt is the time step for Euler Intgration.
+
+    num_steps is the number of steps (of length dt) to roll forward.
+    """
+    if num_steps == 0:
+        return x
+    else:
+        x_dot, R, W = continuous_dynamics(x, u)
+
+        # Precise integration of rotation matrix to preserve orthogonality and det=1 using Exponential Map
+        theta = np.linalg.norm(W + eps)  # add eps to avoid zero norm
+        W_hat = hat_map(W) * dt
+        # Rodrigues' Formula
+        R_new = R @ (np.eye(3) + np.sin(theta) * W_hat + (1 - np.cos(theta)) * np.power(W_hat, 2))
+
+        # Euler Integration for other components of state
+        x_updated = np.concatenate((x[:6] + x_dot[:6] * dt, R_new.flatten(), x[15:] + x_dot[15:] * dt))
+
+        # Recursive call
+        return discrete_dynamics(x_updated, u, dt, num_steps - 1)
+    
+
+def compute_discrete_dynamics_time_step(n, dt_base):
+    """
+    Helper function for `discrete_dynamics` that chops up a potentially large 
+    `dt_nominal` into `num_steps` smaller intervals of size `dt`.
+
+    This helps the discrete time dynamics roll out be more precise while still
+    being able to use computationally-efficient Euler Integration.
+
+    Note: this function cannot be part of `discrete_dynamics` itself because
+    this function is not differentiable.
+
+    n is the step number in the entire N-step trajectory.
+
+    dt_base is the length of the time interval that `dt_nominal` exponentially
+    grows to.
     """
     # https://www.desmos.com/calculator/vegofqyne7
-    beta = 5
-    dt = 1/(beta*n + (1/dt_nominal)) + dt_nominal
+    beta = 1
+    dt_nominal = 1/(beta*n + (1/dt_base)) + dt_base
 
-    x_dot, R, W = continuous_dynamics(x, u)
+    dt_thresh = 0.1  # max allowed time interval to maintain precision in dynamics rollout
 
-    # x_new = x_dot*dt + x  # Euler Integration
-    # return x_new
-
-    # Precise integration of rotation matrix to preserve orthogonality and det=1 using Exponential Map
-    theta = np.linalg.norm(W + eps)  # add eps to prevent input to np.linalg.norm from going to 0 (np.linalg.norm becomes undifferentiable)
-    W_hat = hat_map(W) * dt
-    # Rodrigues' Formula
-    R_new = R @ (np.eye(3) + np.sin(theta) * W_hat + (1 - np.cos(theta)) * np.power(W_hat, 2))
-
-    # Euler Integration for other components of state
-    return np.concatenate((x[:6]+x_dot[:6]*dt, R_new.flatten(), x[15:]+x_dot[15:]*dt))
+    if dt > dt_thresh:
+        num_steps = int(dt_nominal / dt_thresh) + 1
+        dt = dt_nominal / num_steps
+        return dt, num_steps
+    
+    # If dt_nominal is already a small enough time step
+    return dt_nominal, 1
 
 
-def dynamics_rollout(x0, u_trj, dt):
+def dynamics_rollout(x0, u_trj, dt_base):
     """
     Computes x_trj, the state trajectory, given a sequence of control inputs.
 
@@ -95,7 +157,8 @@ def dynamics_rollout(x0, u_trj, dt):
 
     x_trj = [x0]
     for i in range(N):
-        x_trj.append(discrete_dynamics(x_trj[-1], u_trj[i], i, dt))
+        num_steps, dt = compute_discrete_dynamics_time_step(i, dt_base)
+        x_trj.append(discrete_dynamics(x_trj[-1], u_trj[i], dt, num_steps))
 
     return np.array(x_trj)
 
@@ -164,16 +227,21 @@ def cost_trj(pose_goal, x_trj, u_trj):
 
 
 class derivatives:
-    def __init__(self, pose_goal, discrete_dynamics, trajectory_cost, terminal_cost, N, dt):
+    def __init__(self, pose_goal, discrete_dynamics, trajectory_cost, terminal_cost, N, dt_base):
         n_x = 18
         n_u = 4
         self.x_sym = np.array([sym.Variable("x_{}".format(i)) for i in range(n_x)])
         self.u_sym = np.array([sym.Variable("u_{}".format(i)) for i in range(n_u)])
         self.n_sym = np.array([sym.Variable("n")])
+        self.dt_sym = np.array([sym.Variable("dt")])
+        self.num_steps_sym = np.array([sym.Variable("num_steps")])
         x = self.x_sym
         u = self.u_sym
         n = self.n_sym
+        dt = self.dt_sym
+        num_steps = self.num_steps_sym
         self.N = N
+        self.dt_base = dt_base
 
         l = trajectory_cost(pose_goal, x, u, n, N)
         self.l_x = sym.Jacobian([l], x).ravel()
@@ -186,15 +254,19 @@ class derivatives:
         self.l_final_x = sym.Jacobian([l_final], x).ravel()
         self.l_final_xx = sym.Jacobian(self.l_final_x, x)
 
-        f = discrete_dynamics(x, u, n, dt)
+        f = discrete_dynamics(x, u, dt, num_steps)
         self.f_x = sym.Jacobian(f, x)
         self.f_u = sym.Jacobian(f, u)
 
     def stage(self, x, u, n):
+        dt, num_steps = compute_discrete_dynamics_time_step(n, self.dt_base)
+
         # Populate real values of x and u into symbolic variable
         env = {self.x_sym[i]: x[i] for i in range(x.shape[0])}
         env.update({self.u_sym[i]: u[i] for i in range(u.shape[0])})
-        env.update({self.n_sym[0]: n})
+        env.update({self.n[0]: n})
+        env.update({self.dt_sym[0]: dt})
+        env.update({self.num_steps_sym[0]: num_steps})
 
         l_x = sym.Evaluate(self.l_x, env).ravel()
         l_u = sym.Evaluate(self.l_u, env).ravel()
@@ -301,7 +373,7 @@ def expected_cost_reduction(Q_u, Q_uu, k):
     return -Q_u.T.dot(k) - 0.5 * k.T.dot(Q_uu.dot(k))
 
 
-def forward_pass(x_trj, u_trj, k_trj, K_trj, dt):
+def forward_pass(x_trj, u_trj, k_trj, K_trj, dt_base):
     """
     Apply optimal control actions computed in the back pass to find a new
     nominal x and u trajectory.
@@ -320,7 +392,9 @@ def forward_pass(x_trj, u_trj, k_trj, K_trj, dt):
         du = k_trj[n] + K_trj[n] @ dx  #d u[n]* = k + K @ dx[n]
 
         u_trj_new[n,:] = u_trj[n,:] + du
-        x_trj_new[n+1,:] = discrete_dynamics(x_trj_new[n], u_trj_new[n], n, dt)
+
+        dt, num_steps = compute_discrete_dynamics_time_step(n, dt_base)
+        x_trj_new[n+1,:] = discrete_dynamics(x_trj_new[n], u_trj_new[n], dt, num_steps)
 
     return x_trj_new, u_trj_new
 
@@ -360,7 +434,7 @@ def backward_pass(derivs, x_trj, u_trj, regu):
     return k_trj, K_trj, expected_cost_redu
 
 
-def solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, dt, max_iter=200, regu_init=100):
+def solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, dt_base, max_iter=200, regu_init=100):
     """
     x0 is the Drake default [x, y, z, R, P, Y, x_dot, y_dot, z_dot, R_dot, P_dot, Y_dot] state vector.
 
@@ -373,7 +447,7 @@ def solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, dt, max_it
     Rf = euler_to_rotation_matrix(pose_goal[3:6])
     xf = np.concatenate((pose_goal[:3], np.zeros(3), Rf.flatten(), np.zeros(3)))
 
-    derivs = derivatives(pose_goal, discrete_dynamics, trajectory_cost, terminal_cost, N, dt)
+    derivs = derivatives(pose_goal, discrete_dynamics, trajectory_cost, terminal_cost, N, dt_base)
 
     # u_trj = np.random.randn(N - 1, n_u) * 0.0001
     # x_trj = dynamics_rollout(x0, u_trj)
@@ -389,7 +463,7 @@ def solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, dt, max_it
     # Initial Guesses for trajectory
     u_trj = np.ones((N - 1, n_u)) * (-m * g / 4)
     # u_trj = np.random.randn(N - 1, n_u) * 0.0001
-    x_trj = dynamics_rollout(x0, u_trj, dt)
+    x_trj = dynamics_rollout(x0, u_trj, dt_base)
     total_cost = cost_trj(pose_goal, x_trj, u_trj)
 
     regu = regu_init
@@ -407,7 +481,7 @@ def solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, dt, max_it
     for i in range(max_iter):
         # Backward and forward pass
         k_trj, K_trj, expected_cost_redu = backward_pass(derivs, x_trj, u_trj, regu)
-        x_trj_new, u_trj_new = forward_pass(x_trj, u_trj, k_trj, K_trj, dt)
+        x_trj_new, u_trj_new = forward_pass(x_trj, u_trj, k_trj, K_trj, dt_base)
 
         # Evaluate new trajectory
         total_cost = cost_trj(pose_goal, x_trj_new, u_trj_new)
@@ -447,7 +521,7 @@ def solve_trajectory(x0, pose_goal, N):
     min_cost = np.inf
     min_cost_traj = []
     min_cost_time_interval = 0
-    for i in np.linspace(0.025, 0.1, 3):
+    for i in np.linspace(0.025, 0.2, 3):
         x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace = solve_trajectory_fixed_timesteps_fixed_interval(x0, pose_goal, N, i)
         print(f"=====================================cost (dt={i}): {cost_trace[-1]}=====================================")
         if cost_trace[-1] <= min_cost:
