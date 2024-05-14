75f241859e4d60ccf891ab28f66dcc9813da5d0b
Michaelszeng <michaelszeng@gmail.com>
2024-05-14 03:14:47 -0400

    i tried

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ from pydrake.all import (
     Simulator,
     StartMeshcat,
     namedview,
-    JointSliders
+    JointSliders,
+    FiniteHorizonLinearQuadraticRegulatorOptions,
+    MakeFiniteHorizonLinearQuadraticRegulator,
+    PiecewisePolynomial
 )
 from underactuated.scenarios import AddFloatingRpyJoint
+from underactuated import ConfigureParser
 
 import numpy as np
 import os
@@ -28,7 +32,8 @@ import yaml
 from src.utils import *
 from src.ddp import solve_trajectory, solve_trajectory_fixed_timesteps_fixed_interval
 # from src.se3_leaf import SE3Controller
-from src.controller import SE3Controller
+# from src.controller import SE3Controller
+from src.controllerv2 import SE3Controller
 
 meshcat = StartMeshcat()
 
@@ -48,118 +53,11 @@ pose_goal = np.array([0, 0, 0, 0, 0, 1.57])
 
 
 ################################################################################
-##### Diagram Setup
-################################################################################
-builder = DiagramBuilder()
-plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.005)
-parser = Parser(plant)
-(model_instance,) = parser.AddModelsFromUrl("package://drake/examples/quadrotor/quadrotor.urdf")
-
-# Set up the floating base type for the quadrotor.
-# NOTE: THE ORDER OF ROLL PITCH YAW IN THE STATE REPRESETATION IS rz,ry,rxs
-AddFloatingRpyJoint(
-    plant,
-    plant.GetFrameByName("base_link"),
-    model_instance,
-    use_ball_rpy=False
-)
-
-# Add visual quadrotor to show the desired pose of the main quadrotor
-(visual_quadrotor_model_instance,) = parser.AddModelsFromUrl(f"file://{os.path.abspath('visual_quadrotor.urdf')}")
-visual_quadrotor_pose = RigidTransform(RollPitchYaw(pose_goal[3:]), pose_goal[:3])
-
-plant.Finalize()
-
-# Set up the propellers to generate spatial force on quadrotor
-body_index = plant.GetBodyByName("base_link").index()
-kF = 1.0  # Force input constant
-# Propellors 1 and 3 rotate CW, 2 and 4 rotate CCW
-# prop_info = [
-#     PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), L/np.sqrt(2), 0]), kF, -kM),
-#     PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), L/np.sqrt(2), 0]), kF, kM),
-#     PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, -kM),
-#     PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, kM),
-# ]
-prop_info = [
-    PropellerInfo(body_index, RigidTransform([L, 0, 0]), kF, kM),
-    PropellerInfo(body_index, RigidTransform([0, L, 0]), kF, -kM),
-    PropellerInfo(body_index, RigidTransform([-L, 0, 0]), kF, kM),
-    PropellerInfo(body_index, RigidTransform([0, -L, 0]), kF, -kM),
-]
-propellers = builder.AddSystem(Propeller(prop_info))
-builder.Connect(
-    propellers.get_output_port(),
-    plant.get_applied_spatial_force_input_port()
-)
-builder.Connect(
-    plant.get_body_poses_output_port(),
-    propellers.get_body_poses_input_port()
-)
-
-se3_controller = builder.AddSystem(SE3Controller())
-state_converter = builder.AddSystem(StateConverter())
-desired_state_source = builder.AddSystem(TrajectoryDesiredStateSource())
-builder.Connect(
-    plant.GetOutputPort("quadrotor_state"),
-    state_converter.GetInputPort("drone_state")
-)
-builder.Connect(
-    state_converter.GetOutputPort("drone_state_se3"),
-    se3_controller.GetInputPort("drone_state")
-)
-builder.Connect(
-    desired_state_source.GetOutputPort("trajectory_desired_state"),
-    se3_controller.GetInputPort("x_trajectory")
-)
-builder.Connect(
-    se3_controller.GetOutputPort("controller_output"),
-    propellers.get_command_input_port()
-)
-
-
-### TEMPORARY: CONSTANT CONTROL INPUT = mg ###
-# g = plant.gravity_field().gravity_vector()[2]
-# constant_thrust_command = [-m * g / 4] * 4
-# constant_input_source = builder.AddSystem(ConstantVectorSource(constant_thrust_command))
-# builder.Connect(constant_input_source.get_output_port(), propellers.get_command_input_port())
-
-MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
-
-# Build the diagram and create an svg
-diagram = builder.Build()
-diagram_visualize_connections(diagram, "diagram.svg")
-
-
-################################################################################
-##### Simulation Setup
-################################################################################
-simulator = Simulator(diagram)
-context = simulator.get_mutable_context()
-plant_context = plant.GetMyMutableContextFromRoot(context)
-propellers_context = propellers.GetMyMutableContextFromRoot(context)
-desired_state_source_context = desired_state_source.GetMyMutableContextFromRoot(context)
-
-# Set visual quadrotor position
-plant.SetFreeBodyPose(plant_context, plant.GetBodyByName("visual_quadrotor_base_link"), visual_quadrotor_pose)
-visual_quadrotor_joint_idx = plant.GetJointIndices(visual_quadrotor_model_instance)[0]
-visual_quadrotor_joint = plant.get_joint(visual_quadrotor_joint_idx)  # Joint object
-visual_quadrotor_joint.Lock(plant_context)
-
-# Set initial state
-# plant.SetFreeBodyPose(plant_context, plant.GetBodyByName("base_link"), RigidTransform([0, 0, 1]))
-plant.GetJointByName("x").set_translation(plant_context, x0)
-plant.GetJointByName("y").set_translation(plant_context, y0)
-plant.GetJointByName("z").set_translation(plant_context, z0)
-plant.GetJointByName("rx").set_angle(plant_context, rx0)  # Roll
-plant.GetJointByName("ry").set_angle(plant_context, ry0)  # Pitch
-plant.GetJointByName("rz").set_angle(plant_context, rz0)  # Yaw
-
-################################################################################
 ##### Run Trajectory Optimization
 ################################################################################
 # Solve for trajectory
 N=20  # Number of time steps in trajectory
-# x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace, dt, dt_array, final_translation_error, final_rotation_error = solve_trajectory(plant.get_state_output_port().Eval(plant_context), pose_goal, N)
+# x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace, dt, dt_array, final_translation_error, final_rotation_error = solve_trajectory(np.array([x0, y0, z0, rz0, ry0, rx0, 0, 0, 0, 0, 0, 0]), pose_goal, N)
 
 # print(f"{dt=}\n")
 # print(f"{dt_array=}\n")
@@ -321,8 +219,148 @@ pos_3d_matrix = x_trj[:,:3].T
 # print(f"{pos_3d_matrix.T=}")
 meshcat.SetLine("ddp traj", pos_3d_matrix)
 
-desired_state_source.set_time_intervals(dt_array)
-desired_state_source.GetInputPort("trajectory").FixValue(desired_state_source_context, x_trj)
+
+
+################################################################################
+##### Diagram Setup
+################################################################################
+builder = DiagramBuilder()
+quadrotor_diagram_builder = DiagramBuilder()
+plant, scene_graph = AddMultibodyPlantSceneGraph(quadrotor_diagram_builder, time_step=0.005)
+parser = Parser(plant)
+(model_instance,) = parser.AddModelsFromUrl("package://drake/examples/quadrotor/quadrotor.urdf")
+
+# Set up the floating base type for the quadrotor.
+# NOTE: THE ORDER OF ROLL PITCH YAW IN THE STATE REPRESETATION IS rz,ry,rxs
+AddFloatingRpyJoint(
+    plant,
+    plant.GetFrameByName("base_link"),
+    model_instance,
+    use_ball_rpy=False
+)
+
+# Add visual quadrotor to show the desired pose of the main quadrotor
+(visual_quadrotor_model_instance,) = parser.AddModelsFromUrl(f"file://{os.path.abspath('visual_quadrotor.urdf')}")
+visual_quadrotor_pose = RigidTransform(RollPitchYaw(pose_goal[3:]), pose_goal[:3])
+
+plant.Finalize()
+
+
+# Set up the propellers to generate spatial force on quadrotor
+body_index = plant.GetBodyByName("base_link").index()
+kF = 1.0  # Force input constant
+# Propellors 1 and 3 rotate CW, 2 and 4 rotate CCW
+# prop_info = [
+#     PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), L/np.sqrt(2), 0]), kF, -kM),
+#     PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), L/np.sqrt(2), 0]), kF, kM),
+#     PropellerInfo(body_index, RigidTransform([-L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, -kM),
+#     PropellerInfo(body_index, RigidTransform([L/np.sqrt(2), -L/np.sqrt(2), 0]), kF, kM),
+# ]
+prop_info = [
+    PropellerInfo(body_index, RigidTransform([L, 0, 0]), kF, kM),
+    PropellerInfo(body_index, RigidTransform([0, L, 0]), kF, -kM),
+    PropellerInfo(body_index, RigidTransform([-L, 0, 0]), kF, kM),
+    PropellerInfo(body_index, RigidTransform([0, -L, 0]), kF, -kM),
+]
+propellers = quadrotor_diagram_builder.AddSystem(Propeller(prop_info))
+quadrotor_diagram_builder.Connect(
+    propellers.get_output_port(),
+    plant.get_applied_spatial_force_input_port()
+)
+quadrotor_diagram_builder.Connect(
+    plant.get_body_poses_output_port(),
+    propellers.get_body_poses_input_port()
+)
+MeshcatVisualizer.AddToBuilder(quadrotor_diagram_builder, scene_graph, meshcat)
+quadrotor_diagram_builder.ExportInput(propellers.get_command_input_port())
+quadrotor_diagram_builder.ExportOutput(plant.GetOutputPort("state"))
+quadrotor_diagram = quadrotor_diagram_builder.Build()
+
+builder.AddSystem(quadrotor_diagram)
+
+Q = np.diag(np.concatenate(([10] * 6, [1] * 6)))
+R = np.eye(4)
+options = FiniteHorizonLinearQuadraticRegulatorOptions()
+options.Qf = Q
+break_points = np.concatenate(([0], np.cumsum(dt_array)))
+x_trj = np.vstack((x_trj, x_trj[np.shape(x_trj)[0]-1]))  # Repeat last state at very end of trajectory
+options.x0 = PiecewisePolynomial.FirstOrderHold(break_points, x_trj.T)
+u_trj = np.vstack((u_trj, u_trj[np.shape(u_trj)[0]-1], u_trj[np.shape(u_trj)[0]-1]))  # Repeat last command 2x at very end of trajectory
+options.u0 = PiecewisePolynomial.FirstOrderHold(break_points, u_trj.T)
+controller = builder.AddSystem(
+    MakeFiniteHorizonLinearQuadraticRegulator(
+        system=quadrotor_diagram,
+        context=quadrotor_diagram.CreateDefaultContext(),
+        t0=options.u0.start_time(),
+        tf=options.u0.end_time(),
+        Q=Q,
+        R=R,
+        options=options,
+    )
+)
+builder.Connect(controller.get_output_port(), quadrotor_diagram.get_command_input_port())
+builder.Connect(quadrotor_diagram.GetOutputPort("state"), controller.get_input_port())
+
+
+# se3_controller = builder.AddSystem(SE3Controller())
+# state_converter = builder.AddSystem(StateConverter())
+# desired_state_source = builder.AddSystem(TrajectoryDesiredStateSource())
+# builder.Connect(
+#     plant.GetOutputPort("quadrotor_state"),
+#     state_converter.GetInputPort("drone_state")
+# )
+# builder.Connect(
+#     state_converter.GetOutputPort("drone_state_se3"),
+#     se3_controller.GetInputPort("drone_state")
+# )
+# builder.Connect(
+#     desired_state_source.GetOutputPort("trajectory_desired_state"),
+#     se3_controller.GetInputPort("x_trajectory")
+# )
+# builder.Connect(
+#     se3_controller.GetOutputPort("controller_output"),
+#     propellers.get_command_input_port()
+# )
+
+
+### TEMPORARY: CONSTANT CONTROL INPUT = mg ###
+# g = plant.gravity_field().gravity_vector()[2]
+# constant_thrust_command = [-m * g / 4] * 4
+# constant_input_source = builder.AddSystem(ConstantVectorSource(constant_thrust_command))
+# builder.Connect(constant_input_source.get_output_port(), propellers.get_command_input_port())
+
+# Build the diagram and create an svg
+diagram = builder.Build()
+diagram_visualize_connections(diagram, "diagram.svg")
+
+
+################################################################################
+##### Simulation Setup
+################################################################################
+simulator = Simulator(diagram)
+context = simulator.get_mutable_context()
+plant_context = plant.GetMyMutableContextFromRoot(context)
+propellers_context = propellers.GetMyMutableContextFromRoot(context)
+# desired_state_source_context = desired_state_source.GetMyMutableContextFromRoot(context)
+
+# Set visual quadrotor position
+plant.SetFreeBodyPose(plant_context, plant.GetBodyByName("visual_quadrotor_base_link"), visual_quadrotor_pose)
+visual_quadrotor_joint_idx = plant.GetJointIndices(visual_quadrotor_model_instance)[0]
+visual_quadrotor_joint = plant.get_joint(visual_quadrotor_joint_idx)  # Joint object
+visual_quadrotor_joint.Lock(plant_context)
+
+# Set initial state
+# plant.SetFreeBodyPose(plant_context, plant.GetBodyByName("base_link"), RigidTransform([0, 0, 1]))
+plant.GetJointByName("x").set_translation(plant_context, x0)
+plant.GetJointByName("y").set_translation(plant_context, y0)
+plant.GetJointByName("z").set_translation(plant_context, z0)
+plant.GetJointByName("rx").set_angle(plant_context, rx0)  # Roll
+plant.GetJointByName("ry").set_angle(plant_context, ry0)  # Pitch
+plant.GetJointByName("rz").set_angle(plant_context, rz0)  # Yaw
+
+
+# desired_state_source.set_time_intervals(dt_array)
+# desired_state_source.GetInputPort("trajectory").FixValue(desired_state_source_context, x_trj)
 
 
 # Run the simulation
--- src/controllerv2.py
+++ src/controllerv2.py
@@ -9,51 +9,12 @@ import numpy as np
 from src.utils import *
 
 
-def attitude_errors(R, Rd, W, Wd):
-    eR = 0.5*vee_map(Rd.T.dot(R) - R.T.dot(Rd))
-    eW = W - R.T.dot(Rd.dot(Wd))
-    return (eR, eW)
-
-def position_errors(x, xd, v, vd):
-    ex = x - xd
-    ev = v - vd
-    return (ex, ev)
-
-def get_Rc(A, A_dot, A_2dot, b1d, b1d_dot, b1d_ddot):
-    # move this as a function
-    norm_A = np.linalg.norm(A)
-    b3c = - A/norm_A
-    b3c_dot = - A_dot/norm_A + ( np.dot(A, A_dot)*A )/norm_A**3
-    b3c_2dot = - A_2dot/norm_A + ( 2*np.dot(A*A_dot,A_dot) )/norm_A**3 + np.dot( A_dot* A_dot + A*A_2dot ,A)/norm_A**3 - 3*np.dot((A*A_dot)**2,A)/norm_A**5
-
-    b_ = np.cross(b3c, b1d)
-    b_norm = np.linalg.norm(b_)
-    b_dot = np.cross(b3c_dot, b1d) + np.cross(b3c, b1d_dot)
-    b_2dot = np.cross(b3c_2dot, b1d) + 2*np.cross(b3c_dot, b1d_dot) + np.cross(b3c, b1d_ddot)
-
-    b1c = -np.cross( b3c, b_ )/b_norm
-    b1c_dot = -( np.cross(b3c_dot, b_) + np.cross(b3c, b_dot) )/b_norm + np.cross(b3c, b_)*(b_dot* b_)/b_norm**3
-
-    # intermediate steps to calculate b1c_2dot
-    m_1 = ( np.cross(b3c_2dot, b_) + 2*np.cross(b3c_dot, b_dot) + np.cross(b3c, b_2dot) )/b_norm
-    m_2 = ( np.cross(b3c_dot, b_) + np.cross(b3c, b_dot) )*np.dot(b_dot, b_)/b_norm**3
-    m_dot = m_1 - m_2
-    n_1 = np.cross(b3c, b_)*np.dot(b_dot, b_)
-    n_1dot = ( np.cross(b3c_dot, b_) + np.cross(b3c, b_dot) )*np.dot(b_dot, b_) + np.cross(b3c, b_)*( np.dot(b_2dot, b_)+np.dot(b_dot, b_dot) )
-    n_dot = n_1dot/b_norm**3 - 3*n_1*np.dot(b_dot, b_)/b_norm**5
-    b1c_2dot = -m_dot + n_dot
-
-    Rc = np.reshape([b1c, np.cross(b3c, b1c), b3c],(3,3)).T
-    Rc_dot = np.reshape([b1c_dot, ( np.cross(b3c_dot, b1c) + np.cross(b3c, b1c_dot) ), b3c_dot],(3,3)).T
-    Rc_2dot = np.reshape( [b1c_2dot, ( np.cross(b3c_2dot, b1c) + np.cross(b3c_dot, b1c_dot) + np.cross(b3c_dot, b1c_dot) + np.cross(b3c, b1c_2dot) ), b3c_2dot],(3,3)).T
-    Wc = vee_map(Rc.T.dot(Rc_dot))
-    Wc_dot= vee_map( Rc_dot.T.dot(Rc_dot) + Rc.T.dot(Rc_2dot))
-    return (Rc, Wc, Wc_dot)
-
-
 """
 Quadrotor state is represented as the (18,) vector:
 [x, y, z, x_dot, y_dot, z_dot, R1, R2, R3, R4, R5, R6, R7, R8, R9, W1, W2, W3].T
+
+Note: R is rotation from body-fixed frame to world frame ({}^W R^B)
+Note: W is w.r.t. body-fixed frame
 """
 class DirtyDerivative:
     def __init__(self, order, tau=0.05):
@@ -107,7 +68,7 @@ class SE3Controller(LeafSystem):
         # self.kW = 2.54
         self.kx = 16*m
         self.kv = 5.6*m
-        self.kR = 0.001
+        self.kR = 0.01
         self.kW = 0.0
 
         self.prev_desired_state = np.empty((18,))
@@ -116,23 +77,6 @@ class SE3Controller(LeafSystem):
 
         self.xd_ddot = np.zeros(3)
         self.Wd_dot = np.zeros(3)
-
-
-
-
-        self.m = 0.775
-        self.g = 9.81
-        self.J = I
-        self.e3 = np.array([0.,0.,1.])
-        # self.kR = 8.81 # attitude gains
-        # self.kW = 2.54 # attitude gains
-        self.kR = 0 # attitude gains
-        self.kW = 0 # attitude gains
-        self.kx = 16.*self.m # position gains
-        self.kv = 5.6*self.m # position gains
-        self.xd = None
-        self.xd_dot = None
-        self.command = None
 
         
     def CalcOutput(self, context, output):
@@ -149,71 +93,7 @@ class SE3Controller(LeafSystem):
         desired_state = self.get_input_port(1).Eval(context)
         print(desired_state)
 
-        # Drone current state
-        x = drone_state[:3]
-        v = drone_state[3:6]
-        R = drone_state[6:15].reshape(3, 3) @ np.array([[1,0,0],[0,-1,0],[0,0,-1]])  # rotate by 180 deg in x-axis to account for difference in body frame defn.
-        W = np.array([drone_state[15], -drone_state[16], -drone_state[17]])  # negate b2 and b3 angular velocity to account for difference in body frame defn.
-
-        xd = desired_state[:3]
-        xd_dot = desired_state[3:6]
-        xd_2dot = np.array([0, 0, 0])  # TEMPORARY
-        xd_3dot = np.array([0, 0, 0])  # TEMPORARY?
-        xd_4dot = np.array([0, 0, 0])  # TEMPORARY?
-        
-        b1d = desired_state[6:15].reshape(3, 3) @ np.array([1, 0, 0])        # b1d is set by the DDP trajectory
-        b1d_dot = np.array([0, 0, 0])  # TEMPORARY
-        b1d_ddot = np.array([0, 0, 0])  # TEMPORARY?
-
-        (ex, ev) = position_errors(x, xd, v, xd_dot)
-
-        f = np.dot(self.kx*ex + self.kv*ev + self.m*self.g*self.e3 - self.m*xd_2dot, R.dot(self.e3) )
-        W_hat = hat_map(W)
-        R_dot = R.dot(W_hat)
-        x_2dot = self.g*self.e3 - f*R.dot(self.e3)/self.m
-        ex_2dot = x_2dot - xd_2dot
-
-        f_dot = ( self.kx*ev + self.kv*ex_2dot - self.m*xd_3dot).dot(R.dot(self.e3)) + ( self.kx*ex + self.kv*ev + self.m*self.g*self.e3 - self.m*xd_3dot).dot(np.dot(R_dot,self.e3))
-
-        x_3dot = -1/self.m*( f_dot*R + f*R_dot ).dot(self.e3)
-        ex_3dot = x_3dot - xd_3dot
-
-        A = -self.kx*ex - self.kv*ev - self.m*self.g*self.e3 + self.m*xd_2dot
-        A_dot = -self.kx*ev - self.kv*ex_2dot + self.m*xd_3dot
-        A_2dot = -self.kx*ex_2dot - self.kv*ex_3dot + self.m*xd_4dot
-
-        (Rd, Wd, Wd_dot) = get_Rc(A, A_dot, A_2dot , b1d, b1d_dot, b1d_ddot)
-
-        (eR, eW) = attitude_errors( R, Rd, W, Wd )
-        M= -self.kR*eR - self.kW*eW + np.cross(W, self.J.dot(W)) - self.J.dot(W_hat.dot(R.T.dot(Rd.dot(Wd))) - R.T.dot(Rd.dot(Wd_dot)))
-
-
-        # These values match the body frame defn. in Lee et al.
-        net_force_moments_matrix = np.array([[1, 1, 1, 1],
-                                             [0, -L, 0, L],
-                                             [L, 0, -L, 0],
-                                             [-kM, kM, -kM, kM]])
-
-        # print(f"{f=}")
-        # print(f"{M=}")
-
-        u = np.linalg.inv(net_force_moments_matrix) @ np.concatenate(([f], M))
-        u = np.clip(u, -1e2, 1e2)
-        u = np.array([u[0], u[3], u[1], u[2]])  # swap propellors 2 and 4 to account for difference in body frame defn.
-        print(f"{u=}")
-
-        # Set output
-        output.SetFromVector(u)
-        
-        return (f, M)
-
-
-
-
-
-
-
-
+        # desired_state = np.array([-1.4, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0])
 
         # if not np.any(np.isnan(self.prev_desired_state)) and not np.all(np.isclose(desired_state, self.prev_desired_state)):
         #     # New desired_state has been received
@@ -237,14 +117,14 @@ class SE3Controller(LeafSystem):
         # Position/velocity/accelertion desired and error
         xd = desired_state[:3]
         vd = desired_state[3:6]
-        e_x = x - xd
-        e_v = v - vd
-        print(f"{e_x=}")
-        print(f"{e_v=}")
+        ex = x - xd
+        ev = v - vd
+        print(f"{ex=}")
+        print(f"{ev=}")
         xd_ddot = self.xd_ddot  # for convenience so I don't have to repeat `self.`
 
         # Rotation/Rotational velocity/Angular acceleration desired and error
-        A = -self.kx*e_x - self.kv*e_v + m*g*np.array([0,0,1]) + m*xd_ddot  # note that g is negative
+        A = -self.kx*ex - self.kv*ev - m*g*np.array([0,0,1]) + m*xd_ddot  # note that g is negative
         print(f"{A=}")
         b3d = -A / np.linalg.norm(A)                                         # b3d is determined by necessary heading to reach position setpoint
         b1d = desired_state[6:15].reshape(3, 3) @ np.array([1, 0, 0])        # b1d is set by the DDP trajectory
@@ -258,25 +138,28 @@ class SE3Controller(LeafSystem):
         print(f"{Rd=}")
         print(f"{Rd_traj=}")
         Wd = np.array([desired_state[15], -desired_state[16], -desired_state[17]])  # negate b2 and b3 angular velocity to account for difference in body frame defn.
-        e_R = 0.5 * vee_map(Rd.T @ R - R.T @ Rd)
-        e_W = W - R.T @ Rd @ Wd
+        eR = 0.5 * vee_map(Rd.T @ R - R.T @ Rd)
+        eW = W - R.T @ Rd @ Wd  # current angular velocity (in body frame) - desired angular velocity transformed into body frame. This is equivalent to the angular velocty of the rotation matrix Rd.T @ R (from body frame to desired body frame)
         Wd_dot = self.Wd_dot  # for convenience so I don't have to repeat `self.`
 
         f = np.dot(-A, R @ np.array([0, 0, 1]))
-        M = -self.kR*e_R - self.kW*e_W + np.cross(W, I @ W) - I @ (hat_map(W) @ R.T @ Rd @ Wd - R.T @ Rd @ Wd_dot)
+
+        M = -self.kR*np.eye(3) @ eR - self.kW*np.eye(3) @ eW
+
+
+        # M = -self.kR*eR - self.kW*eW + np.cross(W, I @ W) - I @ (hat_map(W) @ R.T @ Rd @ Wd - R.T @ Rd @ Wd_dot)
 
         # These values match the body frame defn. in Lee et al.
         net_force_moments_matrix = np.array([[1, 1, 1, 1],
-                                             [0, -L, 0, L],
-                                             [L, 0, -L, 0],
-                                             [-kM, kM, -kM, kM]])
+                                             [0, L, 0, -L],
+                                             [-L, 0, L, 0],
+                                             [kM, -kM, kM, -kM]])
 
-        # print(f"{f=}")
-        # print(f"{M=}")
+        print(f"{f=}")
+        print(f"{M=}")
 
         u = np.linalg.inv(net_force_moments_matrix) @ np.concatenate(([f], M))
         u = np.clip(u, -1e2, 1e2)
-        # u = np.array([u[0], u[3], u[1], u[2]])  # swap propellors 2 and 4 to account for difference in body frame defn.
         print(f"{u=}")
 
         # Set output
